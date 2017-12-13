95eee661778ec907268e5d6821dc77a8da376449
coolre <coolre@gmail.com>
2017-12-13 22:41:51 +0800

    vue

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # opcms
+python manage.py runserver 0.0.0.0:8000
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py sqlmigrate
+
+
+$ python manage.py startapp polls
+python manage.py createsuperuser
+
+
+organizations
+
+django-guardian
--- apps/organization/models.py
+++ apps/organization/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
-
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 # Create your models here.
 
 
@@ -12,18 +13,25 @@ class CompanyManager(models.Manager):
     # def filter_by_instance(self, instance):
     #     content_type = ContentType.objects.get_for_model(instance.__class__)
     #     obj_id = instance.id
-    #     qs = super(CommentManager, self).filter(content_type=content_type, object_id=obj_id).filter(parent=None)
+    #     qs = super(CompanyManager, self).filter(content_type=content_type, object_id=obj_id).filter(parent=None)
     #     return qs
 
 
 class Company(models.Model):
     title = models.CharField(max_length=500)
+    # content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
+    # object_id = models.PositiveIntegerField()
+    # content_object = GenericForeignKey('content_type', 'object_id')
 
     objects = CompanyManager()
 
     def __str__(self):
         return self.title
+        # if self.parent is not None:
+        #     return ' - '.join((self.title, self.pk))
+        # else:
+        #     return self.title
 
     def get_absolute_url(self):  # get_absolute_url
         # return f"/restaurants/{self.slug}"
